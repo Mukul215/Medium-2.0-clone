3dff258d51d26d3ff3520719152363689463a52a
Mukul Patel <Mukul215@gmail.com>
2022-02-09 16:21:27 -0500

    added header

---
+++ components/Banner.tsx
@@ -0,0 +1,18 @@
+const Banner = () => (
+  <>
+    <div className="space-y-5 px-10">
+      <h1 className="max-w-xl font-serif text-6xl">
+        <span className="underline decoration-black decoration-4">Medium</span>{' '}
+        is a place to write, read, and connect
+      </h1>
+      <h2>
+        It's easy and free to post your thinking on any topic and connect with
+        millions of readers
+      </h2>
+    </div>
+
+    <div></div>
+  </>
+)
+
+export default Banner
